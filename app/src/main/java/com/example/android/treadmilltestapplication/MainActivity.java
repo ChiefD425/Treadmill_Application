72a086baf4007d15c796c43cf361f61ed1eddb11
Fred Deichler <(fred.deichler@gmail.com)>
2016-04-19 12:21:58 -0500

    added calculations, still fixing math

--- app/src/main/java/com/example/android/treadmilltestapplication/MainActivity.java
+++ app/src/main/java/com/example/android/treadmilltestapplication/MainActivity.java
@@ -6,12 +6,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int sex = 0; //if sex = 0 then male, if sex = 1 then female
     String sexString = "";
+    int height = 0;
+    int weight = 0;
+    float time = 0;
+    float incline = 0;
+    float speed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
                         sexString = "Female";
                     } else
                         sexString = "Male";
-                    Log.v("MainActivity", sexString);
 
                 }
             }
@@ -49,5 +55,95 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void calculateResults(View v) {
 
+        double milesPerMinute = 0;
+        double weightInKilograms = 0;
+        double caloriesPerMinute = 0;
+        double totalCalorieBurn = 0;
+        int steps = 0;
+        double distance = 0;
+
+        //grab the height
+        EditText heightTextView = (EditText) findViewById(R.id.height_input);
+//todo fix error with blank input
+        height = Integer.parseInt(heightTextView.getText().toString());
+
+        //grab the weight
+        EditText weightTextView = (EditText) findViewById(R.id.weight_input);
+        weight = Integer.parseInt(weightTextView.getText().toString());
+
+        //grab the time
+        EditText timeTextView = (EditText) findViewById(R.id.time_on_treadmill);
+        time = Float.parseFloat(timeTextView.getText().toString());
+
+        //grab the speed
+        EditText speedTextView = (EditText) findViewById(R.id.speed);
+        speed = Float.parseFloat(speedTextView.getText().toString());
+
+        //grab the incline
+        EditText inclineTextView = (EditText) findViewById(R.id.incline_percentage);
+        incline = Float.parseFloat(inclineTextView.getText().toString());
+        incline = incline / 100;
+
+        milesPerMinute = speed * 26.8;
+        weightInKilograms = weight / 2.2;
+        caloriesPerMinute = oxygenUsed(milesPerMinute) * weightInKilograms;
+        totalCalorieBurn = caloriesPerMinute * time;
+        steps = (int) (stepsPerMile() * time);
+        distance = time * (speed / 60);
+
+        displayResults(totalCalorieBurn, steps, distance);
+
+
+    }
+
+    private void displayResults(double totalCalorieBurn, int steps, double distance) {
+        TextView resultsTextView = (TextView) findViewById(R.id.results);
+
+        String workoutSummary = "";
+        workoutSummary += "Congratulations!";
+        workoutSummary += "\nYou burned " + totalCalorieBurn + " Calories";
+        workoutSummary += "\nYou took " + steps + " Steps";
+        workoutSummary += "\nYou traveled " + distance + " Miles";
+
+        resultsTextView.setText(workoutSummary);
+    }
+
+    private double oxygenUsed(double milesPerMinute) {
+
+        double oxygenUsedCalculation = 0;
+
+        if (speed > 3.7) {
+            oxygenUsedCalculation = (milesPerMinute * 0.2);
+            oxygenUsedCalculation += (milesPerMinute * incline * 0.9);
+            oxygenUsedCalculation += 3.5;
+        } else {
+            oxygenUsedCalculation = (milesPerMinute * 0.1);
+            oxygenUsedCalculation += (milesPerMinute * incline * 1.8);
+            oxygenUsedCalculation += 3.5;
+            Log.v("milesPerMinute", String.valueOf(milesPerMinute));
+            Log.v("oxygenUsed", String.valueOf(oxygenUsedCalculation));
+        }
+
+        return oxygenUsedCalculation;
+    }
+
+    private int stepsPerMile() {
+
+        int stepsPerMileCalculation = 0;
+
+        if (speed >= 5) {
+            stepsPerMileCalculation = 1084;
+            stepsPerMileCalculation += ((143.6 * (60 / speed)) - (13.5 * height));
+            return stepsPerMileCalculation;
+        }
+        if (sexString == "Male") {
+            stepsPerMileCalculation = 1916;
+        } else
+            stepsPerMileCalculation = 1949;
+
+        stepsPerMileCalculation += ((63.4 * (60 / speed)) - (14.1 * height));
+        return stepsPerMileCalculation;
+    }
 }
