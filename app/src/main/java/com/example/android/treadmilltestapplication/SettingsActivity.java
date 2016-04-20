5898b85b18bcf6a9f2a654e9f5cdee51746a2008
Fred Deichler <(fred.deichler@gmail.com)>
2016-04-20 13:50:35 -0500

    working on trying to add coloring to buttons upon selection

--- app/src/main/java/com/example/android/treadmilltestapplication/SettingsActivity.java
+++ app/src/main/java/com/example/android/treadmilltestapplication/SettingsActivity.java
@@ -3,11 +3,17 @@ package com.example.android.treadmilltestapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -21,6 +27,11 @@ public class SettingsActivity extends Activity {
     float time = 0;
     float incline = 0;
     float speed = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
@@ -48,6 +59,9 @@ public class SettingsActivity extends Activity {
         TextView tempMaleText = (TextView) findViewById(R.id.male_button_selected);
         TextView tempFemaleText = (TextView) findViewById(R.id.female_button_selected);
 
+        Button maleButton = (Button) findViewById(R.id.male_button);
+        Button femaleButton = (Button) findViewById(R.id.female_button);
+
         if (sexString.equals("Female")) {
             tempMaleText.setText("");
             tempFemaleText.setText("selected");
@@ -59,6 +73,9 @@ public class SettingsActivity extends Activity {
             tempMaleText.setText("");
         }
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void calculateResults(View v) {
@@ -220,5 +237,45 @@ public class SettingsActivity extends Activity {
         editor.putInt(getString(R.string.WEIGHT), weight);
         editor.apply();
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Settings Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.treadmilltestapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Settings Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.treadmilltestapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
